3d3a7c1289b350b0d234b2442547444418d77899
sienkus <douniasienka@gmail.com>
2025-09-21 22:10:02 +0400

    Remove basePath for user site deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/my-website',
   trailingSlash: true,
   images: {
     unoptimized: true,
